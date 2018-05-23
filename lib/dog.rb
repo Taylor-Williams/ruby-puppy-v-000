4f4721bebcd8686fa268197a6a2475e0b00cdbef
Taylor Williams <taylor@thewilliams.net>
2018-05-23 21:05:57 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,16 @@ class Dog
 
   @@all = []
   attr_accessor :name
-  
+
   def initialize(name)
     @name = name
   end
 
   def self.clear_all
     @@all = []
+  end
+
+  def self.all
+    @@all
   end
 end
